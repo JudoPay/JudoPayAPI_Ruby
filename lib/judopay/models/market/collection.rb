2b500ce81f8791300aa13e409482f9b49efacc25
Michael Hancock <michael.hancock@judopayments.com>
2017-04-04 09:14:24 +0100

    reverting style changes

--- lib/judopay/models/market/collection.rb
+++ lib/judopay/models/market/collection.rb
@@ -4,7 +4,7 @@ module Judopay
   module Market
     class Collection < Judopay::Collection
       @resource_path = 'market/transactions/collections'
-      @valid_api_methods = %i{all create}
+      @valid_api_methods = [:all, :create]
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ RSpec.configure do |config|
   config.include WebMock::API
 
   config.expect_with :rspec do |c|
-    c.syntax = %i{expect should}
+    c.syntax = [:expect, :should]
   end
 end
 
